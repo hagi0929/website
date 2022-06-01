b2f1f0a459f1f338e4ed3403e9884e3010415a3c
pc <ministove3yo@gmail.com>
2022-06-01 22:21:51 +0900

    .

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-document.addEventListener("mousemove", (e) => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-    console.log(mouseX)
-    console.log(mouseY)
-    console.log(15*(mouseX/screen.width))
-    document.getElementById("floor-wrapper").style.transform = "translateX("+(-10*mouseX/screen.width+5)+"px)";
-
-});
+//document.addEventListener("mousemove", (e) => {
+//    const mouseX = e.clientX;
+//    const mouseY = e.clientY;
+//    console.log(mouseX)
+//    console.log(mouseY)
+//    console.log(15*(mouseX/screen.width))
+//    document.getElementById("floor-wrapper").style.transform = "translateX("+(-10*mouseX/screen.width+5)+"px)";
+//});
