67edc5f0012e44faa83f9d7094d92b9ae89536f9
pc <ministove3yo@gmail.com>
2022-06-02 19:59:44 +0900

    .

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-//document.addEventListener("mousemove", (e) => {
-//    const mouseX = e.clientX;
-//    const mouseY = e.clientY;
-//    console.log(mouseX)
-//    console.log(mouseY)
-//    console.log(15*(mouseX/screen.width))
-//    document.getElementById("floor-wrapper").style.transform = "translateX("+(-10*mouseX/screen.width+5)+"px)";
-//});
+document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    console.log(mouseX);
+    console.log(mouseY);
+    console.log("-"+50+(6*(mouseX/screen.width)-3));
+    document.getElementById("floor-wrapper").style.transform = "translateX(-"+(20*mouseX/screen.width-10)+"px)";
+    document.getElementById("spaceman-wrapper").style.transform = "translateY(-"+(50-(6*(mouseY/screen.width)-3))+"%)"+ "translateX(-"+(50-(6*(mouseX/screen.width)-3))+"%)";
+});
