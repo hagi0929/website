00d739af7d9874b0a6e0f89249b81221e8da318a
pc <ministove3yo@gmail.com>
2022-06-03 22:44:25 +0900

    .

--- main.js
+++ main.js
@@ -6,4 +6,5 @@ document.addEventListener("mousemove", (e) => {
     console.log("-"+50+(6*(mouseX/screen.width)-3));
     document.getElementById("floor-wrapper").style.transform = "translateX(-"+(20*mouseX/screen.width-10)+"px)";
     document.getElementById("spaceman-wrapper").style.transform = "translateY(-"+(50-(6*(mouseY/screen.width)-3))+"%)"+ "translateX(-"+(50-(6*(mouseX/screen.width)-3))+"%)";
+
 });
